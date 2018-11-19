8d6c2a032ecdac5fad626e89dd0d72b41e79f301
Raphael do Vale A. Gomes <raphael@dovale.net>
2018-11-19 15:52:04 -0200

    Add lombok dependency

--- src/main/java/net/dovale/okta/springhibernate/spring/entities/Course.java
+++ src/main/java/net/dovale/okta/springhibernate/spring/entities/Course.java
@@ -1,69 +1,20 @@
 package net.dovale.okta.springhibernate.spring.entities;
 
-
+import lombok.*;
 import javax.persistence.*;
 
 @Entity
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Course {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String name;
-    private int workload;
-    private int rate;
+    @NonNull private String name;
+    @NonNull private int workload;
+    @NonNull private int rate;
     @ManyToOne
     @JoinColumn(foreignKey = @ForeignKey(name = "fk_course_teacher"))
-    private Teacher teacher;
-
-    public Course() {
-    }
-
-    public Course(String name, int workload, int rate, Teacher teacher) {
-        this.name = name;
-        this.workload = workload;
-        this.rate = rate;
-        this.teacher = teacher;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getWorkload() {
-        return workload;
-    }
-
-    public void setWorkload(int workload) {
-        this.workload = workload;
-    }
-
-    public int getRate() {
-        return rate;
-    }
-
-    public void setRate(int rate) {
-        this.rate = rate;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Teacher getTeacher() {
-        return teacher;
-    }
-
-    public void setTeacher(Teacher teacher) {
-        this.teacher = teacher;
-    }
-
+    @NonNull private Teacher teacher;
 }
--- src/main/java/net/dovale/okta/springhibernate/spring/entities/Teacher.java
+++ src/main/java/net/dovale/okta/springhibernate/spring/entities/Teacher.java
@@ -1,58 +1,24 @@
 package net.dovale.okta.springhibernate.spring.entities;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class Teacher {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String name;
-    private String pictureURL;
-    private String email;
-
-    public Teacher() {
-    }
-
-    public Teacher(String name, String pictureURL, String email) {
-        this.name = name;
-        this.pictureURL = pictureURL;
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPictureURL() {
-        return pictureURL;
-    }
-
-    public void setPictureURL(String pictureURL) {
-        this.pictureURL = pictureURL;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @NonNull private String name;
+    @NonNull private String pictureURL;
+    @NonNull private String email;
 }
