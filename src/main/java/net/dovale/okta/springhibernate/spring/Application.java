4df42b707c872ffc31b990c182ceb511425a2a5c
Raphael do Vale A. Gomes <raphael@dovale.net>
2018-11-20 21:40:32 -0200

    Adding security

--- src/main/java/net/dovale/okta/springhibernate/spring/Application.java
+++ src/main/java/net/dovale/okta/springhibernate/spring/Application.java
@@ -2,12 +2,29 @@ package net.dovale.okta.springhibernate.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+
+	@Configuration
+	static class OktaOauth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.authorizeRequests().anyRequest().authenticated()
+					.and()
+					.oauth2ResourceServer().jwt();
+		}
 	}
 }
